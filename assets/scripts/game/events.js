f18735591781f87ad55fbbc08c5850116cb17796
MGubernick <MGubernick61@gmail.com>
2020-12-09 21:42:03 -0600

    Fix spelling Error
    -found a spelling error in my function.  Fixed it.

--- assets/scripts/game/events.js
+++ assets/scripts/game/events.js
@@ -6,7 +6,7 @@ const api = require('./api.js')
 const ui = require('./ui.js')
 // const getFormFields = require('./../../../lib/get-form-fields')
 const store = require('./../store.js')
-
+// new game event handler
 const onNewGame = function (event) {
   api.newGame()
     .then(function (response) {
@@ -16,20 +16,24 @@ const onNewGame = function (event) {
     .then(ui.onNewGameSuccess)
     .catch(ui.onNewGameFailure)
 }
-
+// Player starts as x
 const playerPick = 'X'
+
+// const firstPlayer = 'X'
+//
+// const secondPlayer = 'O'
 
 const onSpaceClick = function (event) {
   $(event.target).html(playerPick)
   const cellIndex = $(event.target).data('cell-index')
   console.log(cellIndex)
-
+  // find the game array in the store
   const gameArray = store.game.cells
   console.log(gameArray)
 
   const gameArrayIndex = gameArray[cellIndex]
   console.log(gameArrayIndex)
-
+  // when user clicks - check that the space is empty
   if (gameArrayIndex !== 'X') {
     api.spaceClick(cellIndex, playerPick)
       .then(function (response) {
